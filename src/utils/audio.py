d5233dda764795d6db26e99cd0929850456b7ea7
gabriel lopes <gabriel18.lopes@gmail.com>
2025-08-20 16:38:20 -0300

    add video generator

--- src/utils/audio.py
+++ src/utils/audio.py
@@ -1,4 +1,6 @@
 import wave
+from pydub import AudioSegment
+
 
 def wave_file(filename, pcm, channels=1, rate=24000, sample_width=2):
    with wave.open(filename, "wb") as wf:
@@ -6,3 +8,13 @@ def wave_file(filename, pcm, channels=1, rate=24000, sample_width=2):
       wf.setsampwidth(sample_width)
       wf.setframerate(rate)
       wf.writeframes(pcm)
+
+
+def mp4_file(filename, pcm, channels=1, rate=24000, sample_width=2):
+    audio = AudioSegment(
+        data=pcm,
+        sample_width=sample_width,
+        frame_rate=rate,
+        channels=channels
+    )
+    audio.export(filename, format="mp4")
