8a0ac420c422ab2eba1ee02355d2f7679e422666
gabriel lopes <gabriel18.lopes@gmail.com>
2025-08-21 15:03:37 -0300

    refactore

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ DEFAULT_ASPECT_RATIO = "1:1"
 DEFAULT_N_IMAGES = 2
 
 # gemini text
-DEFAULT_MODEL_TEXT = "gemini-2.5-flash"
+DEFAULT_MODEL_TEXT = "gemini-2.5-pro"
 
 # https://ai.google.dev/gemini-api/docs/thinking
 DEFAULT_THINKING = 0 # SET TO FALSE
